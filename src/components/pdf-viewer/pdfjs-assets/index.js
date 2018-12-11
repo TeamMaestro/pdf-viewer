860945119fa7965552990ef0efe8ccc180258986
Sean Bannigan <bannigan@meetmaestro.com>
2018-12-11 13:37:51 -0500

    feat: preliminary re-styling

--- src/components/pdf-viewer/pdfjs-assets/index.js
+++ src/components/pdf-viewer/pdfjs-assets/index.js
@@ -1,2 +1,3 @@
 require.context('!!file-loader?name=[name].[ext]&outputPath=pdfjs-assets!./', false, /.min.js$/);
 require.context('!!file-loader?name=[name].[ext]&outputPath=pdfjs-assets/locale!./', true, /.properties$/);
+require.context('!!file-loader?name=[name].[ext]&outputPath=pdfjs-assets/fonts!./', true, /.woff$/);
